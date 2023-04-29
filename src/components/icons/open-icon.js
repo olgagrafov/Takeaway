e787a806607d947965f6519d41bbd28742713473
OlgaGraf0 <olgagrafov@gmail.com>
2023-04-29 16:21:08 +0300

    added text component and info card style

--- src/components/icons/open-icon.js
+++ src/components/icons/open-icon.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Platform } from "react-native";
 import { SvgXml as SVG } from "react-native-svg";
 import { SvgXml } from "react-native-svgxml-web";
-import open from "../../../../assets/open";
+import open from "../../../assets/open";
 
 const OpenIcon = ({ width = 24, height = 24 }) => {
   if (Platform.OS === "web") {
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,51 +1,18 @@
 import React from "react";
-import styled from "styled-components/native";
-import { Text, Image } from "react-native";
-import { Card } from "react-native-paper";
-import OpenIcon from "./open-icon";
-import StarIcon from "./star-icon";
-import { Spacer } from "./spacer/spacer.component";
 
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
-const Address = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
+import OpenIcon from "../../../components/icons/open-icon";
+import StarIcon from "../../../components/icons/star-icon";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+import {
+  Icon,
+  RestaurantCard,
+  RestaurantCardCover,
+  Section,
+  SectionEnd,
+  Info,
+  Rating,
+} from "./restaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -67,7 +34,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <RestaurantCard elevation={5}>
         <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
         <Info>
-          <Title>{name}</Title>
+          <Text variant="label">{name}</Text>
           <Section>
             <Rating>
               {ratingArray.map((x, index) => (
@@ -76,22 +43,17 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
             </Rating>
             <SectionEnd>
               {isClosedTemporarily && (
-                <Text variant="label" style={{ color: "red" }}>
-                  CLOSED TEMPORARILY
-                </Text>
+                <Text variant="error">CLOSED TEMPORARILY</Text>
               )}
               <Spacer position="left" size="large">
                 {isOpenNow && <OpenIcon width={20} height={20} />}
               </Spacer>
               <Spacer position="left" size="large">
-                <Image
-                  style={{ width: 15, height: 15 }}
-                  source={{ uri: icon }}
-                />
+                <Icon source={{ uri: icon }} />
               </Spacer>
             </SectionEnd>
           </Section>
-          <Address>{address}</Address>
+          <Text variant="caption">{address}</Text>
         </Info>
       </RestaurantCard>
     </>
