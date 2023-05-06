d068bcb9845d43b4b725a7ad41bed462f347ab88
OlgaGraf0 <olgagrafov@gmail.com>
2023-05-06 16:02:13 +0300

    added Loading

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import { Ionicons } from "@expo/vector-icons";
 
 import { theme } from "./src/infrastructure/theme";
 
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
@@ -60,13 +61,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Tab.Navigator screenOptions={createScreenOptions}>
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-            <Tab.Screen name="Map" component={Map} />
-            <Tab.Screen name="Settings" component={Settings} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator screenOptions={createScreenOptions}>
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Map" component={Map} />
+              <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
