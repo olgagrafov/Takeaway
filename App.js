da55feb0944f844a6294f39526c5d33d81897af5
OlgaGraf0 <olgagrafov@gmail.com>
2023-12-19 13:41:35 +0200

    added  Firebase Authentication

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
+
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
@@ -13,7 +17,35 @@ import { FavouritesContextProvider } from "./src/services/favourites/favourites.
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDalWyIW5Ks7PBOM0r4KahwA8LTAjBtmjE",
+  authDomain: "mealstogo-982b9.firebaseapp.com",
+  projectId: "mealstogo-982b9",
+  storageBucket: "mealstogo-982b9.appspot.com",
+  messagingSenderId: "670118737378",
+  appId: "1:670118737378:web:5a0d2e4b5dddb5d1de35bb",
+};
+
+let app;
+if (initializeApp.length > 0) {
+  app = initializeApp(firebaseConfig);
+}
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      const auth = getAuth(app);
+      signInWithEmailAndPassword(auth, "mo@binni.io", "test123")
+        .then((user) => {
+          console.log(user);
+          setIsAuthenticated(true);
+        })
+        .catch((e) => {
+          console.log("error: " + e);
+        });
+    }, 2000);
+  }, []);
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -26,6 +58,9 @@ export default function App() {
     return null;
   }
 
+  if (!isAuthenticated) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
